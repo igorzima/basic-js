6bf398c301422205f5065c5251ce572b4dd6e4bd
igorzima <redteam122@gmail.com>
2020-02-27 13:05:38 +0300

    feat: dream-team solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,16 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let newArr = [];
+  if (typeof members === "number" || {}.toString.call(members) === "[object Object]" || members === undefined || members === null || members.length == 0 || members === false) {
+    return false;
+  }
+  members.forEach(function(word) {
+    if (typeof word === "number" || {}.toString.call(word) === "[object Object]" || {}.toString.call(word) === "[object Set]" || word === undefined || word === null || word === false || word === true || typeof word === "object") {
+      let x = 1;
+    } else {
+      let a = word.toUpperCase().trim();
+      return newArr.push(a[0]);
+    }
+  });
+  let result = newArr.sort().join("");
+  return result;
 };
